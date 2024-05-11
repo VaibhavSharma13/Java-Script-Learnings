89a32f6fc56a9962d4a37d454ba1b64b76c7c4cf
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-11 10:01:00 +0000

    data types learnings

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+
+const userName = "viabhav"
+let age = 24
+
+console.log(userName, age);
+
+// string - ""
+// number - 5
+// null - standalone value which meand we can assgin it as a value to any variable
+// undefined - when variable decalred but no value assgined
+//symbol - unique value
+
+// all above are primitve data types - contain single values
+
+// other data types 
+// object - {}
+// array - []
+
+console.log(typeof age);
+console.log(typeof null);  // object
