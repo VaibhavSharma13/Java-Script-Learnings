57b33cff19cbbd8707155eea52633b7fc76fa4c9
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-11 10:21:49 +0000

    conversion operations

---
+++ basics/conversionOperations.js
@@ -0,0 +1,30 @@
+
+// let str = "33"
+
+// console.log(typeof str)
+// console.log(typeof (str))  // both ways are right
+
+// let num = Number(str)
+// console.log(typeof num);
+
+
+// what if their is combination of numbers and characters
+
+let anotherString = "33abc"
+let num = Number(anotherString)
+console.log(typeof num);
+console.log(num)
+
+// "33" - number
+// "33abc" - NaN but type is number 
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 - true, 0 - false
+// "" - false
+// "vaibhav" - true
+
