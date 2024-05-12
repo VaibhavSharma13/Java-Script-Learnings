1eb592409e7f18f9395f00884eaff82c0d4ae2b6
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-12 07:17:12 +0000

    comparison file added

---
+++ basics/comparison.js
@@ -0,0 +1,24 @@
+
+// basic comparisons
+// console.log(2>1);
+// console.log(2>=2);
+// console.log(0<5);
+
+
+// not recommended - try to write a clean and good code
+// console.log("2" > 1);
+// console.log(1 == true);
+
+// both equality and comparison operator works diff
+// comparison operator converts null to zero
+// this type of comparison also not recommended
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// strict check - it compare value and data type both
+console.log("2" === 2);
+
+
+
+
