32324b50b0f02d89a9397c8a56c0411e929644fb
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-13 11:36:21 +0000

    added more object methods

--- basics/objects.js
+++ basics/objects.js
@@ -1,4 +1,4 @@
-// singleton object - constractor method to create singleton objects
+// singleton object - constractor method(using new keyword) to create singleton objects
 // const myUser = Object.create(anotherObject) - it take another object as parameter and inherites properties and methods 
 
 // object literals
@@ -51,4 +51,65 @@ user.obj = {
     height: 10,
     length: 20
 }
-console.log(typeof user.obj);
+// console.log(typeof user.obj);
+
+
+//********** Singleton or constructor object *******
+
+const newUser = new Object()
+
+newUser.id = "456xyz"
+newUser.password = "agaffg"
+newUser.isLoggedIn = false
+
+// console.log(newUser);
+
+console.log(newUser.hasOwnProperty('id'));
+console.log(Object.keys(newUser));  // it return array of keys
+
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        firstName: "vaibhav",
+        lastName: "Sharma"
+    }
+}
+// console.log(regularUser);
+// console.log(regularUser.fullName.firstName);
+
+
+const obj1 = {
+    1: 'a',
+    2: 'b'
+}
+const obj2 = {
+    3: 'a',
+    4: 'b'
+}
+
+// const obj3 = {obj1, obj2}
+// const obj3 = Object.assign({}, obj1, obj2)  // first parameter is target so provided an empty object
+
+const obj3 = {...obj1, ...obj2}  // recommended to use - spread method
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "vaibhav@gmail.com"
+    },
+    {
+        name: "bittu"
+    },
+    {
+        age: 24
+    }
+]
+// console.log(users[0].email);
+
+
+
+
+
