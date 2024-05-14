e4a07009b58fa9bd433eaf086ec4039f13adc686
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-14 05:42:35 +0000

    function file changes

--- 02_basics/functions.js
+++ 02_basics/functions.js
@@ -5,4 +5,26 @@ function addition(n1, n2){
 }
 
 // addition  - it gives the function reference
-addition(5, 3)
+// addition(5, 3)
+
+
+// output: undefined just logged in
+// function userLoginMessage(userName){
+//     return `${userName} just logged in`
+// }
+// console.log(userLoginMessage());
+
+
+function userLoginMessage(userName = "vaibhav"){
+    // if(userName === undefined){
+    //     console.log("please enter userName");
+    //     return
+    // }
+
+    if(!userName){
+        console.log("please enter userName");
+        return
+    }
+    return `${userName} just logged in`
+}
+console.log(userLoginMessage("bittu"));
