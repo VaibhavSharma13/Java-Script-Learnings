d21f79c9ff21a273cb5c16b5f0172ee81c56643d
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-14 17:40:27 +0000

    cnditional statements and some operators learned

--- 03_controlFlow/one.js
+++ 03_controlFlow/one.js
@@ -1,2 +1,39 @@
 // if else statement
 
+// falsy values: false, 0, negative numbers, BigInt, "", null, undefined, NAN
+// all other values are true values to enter into if statement
+
+
+// Nulish Coalescing Operator (??): null undefined
+let value
+value = 5 ?? 10   // output: 5
+value = null ?? 25  // output: 25
+value = undefined ?? 50  // output: 50
+// console.log(value);
+
+
+// Terniary Operator - condition ? true : false
+const price = 100
+price <= 80 ? console.log(`less then 80`) : console.log(`more than 80`);
+
+
+
+// switch
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+
+    default:
+        console.log("default case");
+        break;
+}
+
