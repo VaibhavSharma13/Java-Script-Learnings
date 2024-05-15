63625f77cfa11cc89a887cb47f1b6a2fb2733046
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-15 11:24:30 +0000

    advanced loop concepts

--- 03_basics/loops.js
+++ 03_basics/loops.js
@@ -39,7 +39,7 @@ do {
 
 // for of - not works for objects, but works array and map
 // for in - not works for maps, but works for objects and array
-// for each - 
+// for each - high oreder function defined for array (in prototype)
 
 
 // for of loop
@@ -53,4 +53,74 @@ const greetings = "hello world!"
 for (const greet of greetings) {
         // console.log(`Each char is ${greet}`);
 }
+
+
+// forEach loop
+const myArray = [23, 45, 90]
+
+myArray.forEach((value) => {
+    // console.log(`value inside array : ${value}`);
+})
+
+myArray.forEach((value, index, myArray) => {
+    // console.log(value, index, myArray);
+})
+
+
+const coding = [
+    {
+        languageName: "JavaSript",
+        fileName: "js"
+    },
+    {
+        languageName: "Phython",
+        fileName: "py"
+    },
+    {
+        languageName: "C++",
+        fileName: "cpp"
+    }
+]
+
+coding.forEach( (item) => {
+    // console.log(item.languageName);
+} )
+
+
+// forEach - not return anything
+const returnItems = coding.forEach( (item) => {
+    // console.log(item.languageName);
+} )
+
+// console.log(returnItems);  // undefined
+
+
+// ******* if we don't want to use values for now and store values of array 
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const myNums = nums.filter( (item) => item>4 )
+// console.log(myNums);
+
+
+// if we still want to use still use forEach then we can use like below
+const newNums = []
+nums.forEach( (item) => {
+    if(item > 5) newNums.push(item)
+} )
+// console.log(newNums);
+
+
+// reduce function in array
+// its takes call back function as a parameter and this call back fn
+// takes two parameters - accumulator and current value
+const itemValue = [10, 20, 30]
+
+const total = itemValue.reduce( function(acc, curr) {
+    return acc + curr
+}, 100)  // this 100 will be the initial value of accumulator
+// console.log(total);
+
+// using arrow fn
+const myTotal = itemValue.reduce( (acc, curr) => acc + curr, 100 )
+console.log(myTotal);
 
