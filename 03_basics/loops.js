fef3fc02f863b1cd0aac1e70368e6c6977ba22e0
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-15 06:55:54 +0000

    loops file modified

--- 03_basics/loops.js
+++ 03_basics/loops.js
@@ -15,7 +15,7 @@ for (let i = 0; i < 5; i++) {
 }
 
 
-// break and continue keywords
+// break and continue keywords - example not needed
 
 
 // while and do while loops
@@ -29,6 +29,28 @@ while(score < 5){
 
 let value = 10
 do {
-    console.log(` value is ${value}`);
+    // console.log(` value is ${value}`);
     value++
 } while (value < 5);
+
+
+
+// ******* other loops ******
+
+// for of - not works for objects, but works array and map
+// for in - not works for maps, but works for objects and array
+// for each - 
+
+
+// for of loop
+const arr = [1, 2, 3 ,4 , 5]
+
+for (const num of arr) {
+    // console.log(num);
+}
+
+const greetings = "hello world!"
+for (const greet of greetings) {
+        // console.log(`Each char is ${greet}`);
+}
+
