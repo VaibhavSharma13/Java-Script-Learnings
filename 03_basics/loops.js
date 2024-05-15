347c4761a074284781657d08cab2b674c1f80359
Vaibhav Sharma <75234986+VaibhavSharma13@users.noreply.github.com>
2024-05-15 06:18:36 +0000

    loops in js

---
+++ 03_basics/loops.js
@@ -0,0 +1,34 @@
+// for loop
+
+for (let i = 0; i < 5; i++) {
+    const element = i;
+    // console.log(element);
+}
+
+
+// nested loops
+for (let i = 0; i < 5; i++) {
+    // console.log(`outer loop ${i}`);
+    for (let j = 0; j < 5; j++) {
+        // console.log(`inner loop ${j}`);
+    }
+}
+
+
+// break and continue keywords
+
+
+// while and do while loops
+
+let score = 0
+while(score < 5){
+    // console.log(`my score is ${score}`);
+    score ++
+}
+
+
+let value = 10
+do {
+    console.log(` value is ${value}`);
+    value++
+} while (value < 5);
